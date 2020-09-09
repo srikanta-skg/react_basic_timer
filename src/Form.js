8e389fe4abc5284dcb2e440bb7863a8c343de820
srikanta gowda <srikantagowda07@gmail.com>
2020-09-09 16:45:12 +0530

    added Form.js file

---
+++ src/Form.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Form(props) {
+    console.log(props)
+    let minutes = props.obj.minutes;
+    let seconds = props.obj.seconds;
+    let icon_Click = props.obj.icon_Click;
+    let show_Number = props.obj.show_Number;
+
+    return (
+        <form  > 
+        <div className = "number" >  
+             <div>
+                 <p className = "text_color small"> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</p> 
+                 { show_Number == true ?  <input type="number"  name = "minutes" className = "input_taker" placeholder = 'Enter Number' onChange={props.myChangeHandler2} ></input> :             
+                  <div>  { minutes === 0 && seconds === 0  ? <h1 className = "text_color">0</h1> : <h1 className = "text_color">Time: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1> } </div>  }
+                     <div> <br></br> <br></br><br></br></div>
+             </div>
+            <div className = "icons">  
+                   { icon_Click == true ? <input className = "start_button" type="button" onClick  = {props.myChangeHandler} /> : 
+                   <input className = "pause_button"  type="button"  value = " " onClick  = {props.myChangeHandler3} />     }           
+            <label onSubmit={props.myChangeHandler}>
+             <button className = "restart_button" ></button> </label>       
+             <div> <br></br> </div>
+           </div>
+        </div>
+          </form>
+     )
+ }
+
+
+export default Form;
+
+
+
+
