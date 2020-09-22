746d0a6dd3a33b15116c8f05babdba58a8094424
srikanta gowda <srikantagowda07@gmail.com>
2020-09-22 15:10:00 +0530

    index is provided with store using provide redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider }  from 'react-redux'
+import { createStore } from 'redux';
+import productReducer from './Reducer'
+
+const store = createStore(productReducer);
 
 ReactDOM.render(
+  <Provider store ={store} > 
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
