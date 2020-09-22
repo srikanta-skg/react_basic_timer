5f46f61aeb45098e0d08aa3e58afd879ebd24608
srikanta gowda <srikantagowda07@gmail.com>
2020-09-22 15:08:30 +0530

    Timer.js refactored with redux

--- src/Timer.js
+++ src/Timer.js
@@ -1,102 +1,90 @@
-import React from 'react';
-import Form from './Form.js';
+import React from "react";
+import * as Redux from "react-redux";
+import Form from "./Form.js";
 
 class Timer extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        minutes: 0,
-        seconds: 0,
-        title: "React timer",
-        icon_Click: true,
-        show_Number: true,
-      }
-      this.start_handeler = this.start_handeler.bind(this)
-      this.event_handeler = this.event_handeler.bind(this);
-      this.stop_handeler = this.stop_handeler.bind(this);
-    }
+  _tick = () => {
+    this.props.start();
+  };
 
-   componentDidMount(){
-     console.log("dom has mounted into HTML file")
-   }
+  event_handeler = (event) => {
+    var value = event.target.value;
+    var min = Math.floor(value / 60);
+    var sec = value % 60;
+    this.props.addTime(min, sec);
+  };
 
-   event_handeler = (event) => {
-        var value = event.target.value
-        var min = Math.floor(value / 60);
-        var sec = value % 60;
-        this.setState({
-            [event.target.name]: min
-        });
-        this.setState({
-            seconds: sec
-        });
+  start_handeler = () => {
+    if (this.props.seconds <= 0.1 && this.props.minutes <= 0) {
+      alert("enter value greater then 1");
+      return;
     }
+    this.props.icon_Click();
+    this.timer = setInterval(this._tick, 1000);
+  };
 
-    start_handeler = () => {
-      if ( this.state.seconds <= 0.1  &&  this.state.minutes <= 0 ) {
-        alert("enter value greater then 1")
-        return;
-      }
-        
-      this.setState({
-        icon_Click: false
-      })
-      this.setState({
-        show_Number: false
-      })
+  rest_handeler = () => {
+    this.props.reset();
+    clearInterval(this.timer);
+    this.props.show_Number();
+  };
 
-      this.myInterval = setInterval(() => {
-
-    
-        const {
-          seconds,
-          minutes
-        } = this.state
-        if (seconds > 0) {
-          this.setState(({
-            seconds
-          }) => ({
-            seconds: seconds - 1
-          }))
-        }
-        if (seconds === 0) {
-          if (minutes === 0) {
-            clearInterval(this.myInterval)
-          } else {
-            this.setState(({
-              minutes
-            }) => ({
-              minutes: minutes - 1,
-              seconds: 59
-            }))
-          }
-        }
-      }, 1000)
-    }
-    componentDidUpdate() {
-      console.log("updated")
-    }
-    stop_handeler = () => {
-      clearInterval(this.myInterval)
-      this.setState({
-        icon_Click: true
-      })
-      this.setState({
-        show_Number: true
-      })
-    }
+  stop_handeler = () => {
+    clearInterval(this.timer);
+    this.props.pause();
+    this.props.show_Number();
+  };
 
   render() {
-      return (
-        <div className = "container" >
-       <Form obj ={this.state} 
-       start_handeler =  { this.start_handeler }
-       event_handeler = { this.event_handeler }
-       stop_handeler =  { this.stop_handeler }
-       />       
-        </div>
-      )
+    if (this.props.minutes === 0 && this.props.seconds === 0) {
+      clearInterval(this.timer);
+    }
+    return (
+      <div className="container">
+        <Form
+          obj={this.props}
+          start_handeler={this.start_handeler}
+          event_handeler={this.event_handeler}
+          stop_handeler={this.stop_handeler}
+          rest_handeler={this.rest_handeler}
+        />
+      </div>
+    );
   }
 }
 
-export default Timer;
+const mapStateToProps = (state) => ({
+  icon_Click: state.icon_Click,
+  onClick: state.onClick,
+  show_Number: state.show_Number,
+  count: state.count,
+  Logging: state.Logging,
+  minutes: state.minutes,
+  seconds: state.seconds,
+});
+
+const mapsDispatchToProps = (dispatch) => {
+  return {
+    addTime: (min, sec) => {
+      dispatch({ type: "ADD", min: min, sec: sec });
+    },
+
+    start: () => {
+      dispatch({ type: "PLAY" });
+    },
+    pause: () => {
+      dispatch({ type: "PAUSE" });
+    },
+    reset: () => {
+      dispatch({ type: "RESET" });
+    },
+    icon_Click: () => {
+      dispatch({ type: "CLICKED" });
+    },
+    show_Number: () => {
+      dispatch({ type: "SHOW_NUMBER" });
+    },
+  };
+};
+
+export default Redux.connect(mapStateToProps, mapsDispatchToProps)(Timer);
