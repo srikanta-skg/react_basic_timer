8905f3f480b6f0154e842a90ede73fc502e3ae7d
srikanta gowda <srikantagowda07@gmail.com>
2020-09-22 15:07:51 +0530

    Foem.js Changed styling and fixed code variables name

--- src/Form.js
+++ src/Form.js
@@ -1,36 +1,67 @@
-import React from 'react';
+import React from "react";
 
 function Form(props) {
-    console.log(props)
-    let minutes = props.obj.minutes;
-    let seconds = props.obj.seconds;
-    let icon_Click = props.obj.icon_Click;
-    let show_Number = props.obj.show_Number;
+  let minutes = props.obj.minutes;
+  let seconds = props.obj.seconds;
+  let icon_Click = props.obj.onClick;
+  let show_Number = props.obj.Logging;
 
-    return (
-        <form  > 
-        <div className = "number" >  
-             <div>
-                 <p className = "text_color small"> {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</p> 
-                 { show_Number == true ?  <input type="number"  name = "minutes" className = "input_taker" placeholder = 'Enter Number' onChange={props.event_handeler} ></input> :             
-                  <div>  { minutes === 0 && seconds <= 10  ? <h1 className = "text_color text_color2">Time: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1> : <h1 className = "text_color">Time: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1> } </div>  }
-                     <div> <br></br> <br></br><br></br></div>
-             </div>
-            <div className = "icons">  
-                   { icon_Click == true ? <input className = "start_button" type="button" onClick  = {props.start_handeler} /> : 
-                   <input className = "pause_button"  type="button"  value = " " onClick  = {props.stop_handeler} />     }           
-            <label onSubmit={props.start_handeler}>
-             <button className = "restart_button" ></button> </label>       
-             <div> <br></br> </div>
-           </div>
+  return (
+    <form>
+      <div className="number">
+        <div>
+          <p className="text_color small">
+            {" "}
+            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+          </p>
+          {show_Number === true ? (
+            <input
+              type="number"
+              name="minutes"
+              className="input_taker"
+              placeholder="Enter Number"
+              onChange={props.event_handeler}
+            ></input>
+          ) : (
+            <div>
+              {" "}
+              {minutes === 0 && seconds <= 10 ? (
+                <h1 className="text_color text_colorRed">
+                  Time: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+                </h1>
+              ) : (
+                <h1 className="text_color">
+                  Time: {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+                </h1>
+              )}{" "}
+            </div>
+          )}
         </div>
-      </form>
-     )
- }
+        <div className="icons">
+          {icon_Click === true ? (
+            <input
+              className="start_button"
+              type="button"
+              onClick={props.start_handeler}
+            />
+          ) : (
+            <input
+              className="start_button pause_button"
+              type="button"
+              value=" "
+              onClick={props.stop_handeler}
+            />
+          )}
 
+          <span onClick={props.rest_handeler} className="start_button restart_button"></span>
+          <div>
+            {" "}
+            <br></br>{" "}
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
 
-export default Form;
-
-
-
-
+export default Form
